65f8742f173d8f3ca149c6a4571f98ec715c7573
Siddhu <143573788+SiddhuAL@users.noreply.github.com>
2023-12-02 15:43:43 +0530

    Update Week 5 - Arrays - Transpose using pointers.cpp

--- Week 5 - Arrays - Transpose using pointers.cpp
+++ Week 5 - Arrays - Transpose using pointers.cpp
@@ -2,43 +2,51 @@
 void matrix_print(int,int,int *);
 int main()
 {
-	int m,n;
-	printf("Enter dimensions for matrix (ROWS,COLUMNS):");
-	scanf("%d %d",&m,&n);
-	int a[m][n];
-	for(int i=0;i<m;i++)
+	int p =0;
+	while(p==0)
 	{
-		printf("\nFor row %d :",i+1);
-		for(int j=0;j<n;j++)
+		int m,n;
+		printf("Enter dimensions for matrix (ROWS,COLUMNS):");
+		scanf("%d %d",&m,&n);
+		if(m<0||n<0)
 		{
-			printf("\n\tEnter element %d :",j+1);
-			scanf("%d",&a[i][j]);
+			printf("You've entered negative values...\nEnter again\n\n");
+			p = 0;
+		}
+		else
+		{
+			int a[m][n];
+			for(int i=0;i<m;i++)
+			{
+				printf("\nFor row %d :",i+1);
+				for(int j=0;j<n;j++)
+				{
+					printf("\n\tEnter element %d :",j+1);
+					scanf("%d",&a[i][j]);
+				}
+			}
+			int *c;
+			c=&a[0][0];
+			printf("The matrix is :\n");
+			matrix_print(m,n,c);
+			int b[n][m];
+			for(int i=0;i<n;i++)
+			{
+				for(int j=0;j<m;j++)
+				b[i][j]=a[j][i];
+			}
+			c=&b[0][0];
+			printf("\nThe transpose of matrix is :\n");
+			matrix_print(n,m,c);
+			char ans;
+			printf("\nYou want to continue(Y/n):");
+			scanf(" %c",&ans);
+			if(ans=='Y'||ans=='y')
+			p=0;
+			else 
+			p=1;
 		}
 	}
-	int *c;
-	c=&a[0][0];
-	printf("The matrix is :\n");
-	matrix_print(m,n,c);
-	int b[n][m];
-	for(int i=0;i<n;i++)
-	{
-		for(int j=0;j<m;j++)
-		b[i][j]=a[j][i];
-	}
-	c=&b[0][0];
-	printf("\nThe transpose of matrix is :\n");
-	matrix_print(n,m,c);
 	return 0;
 }
-
-void matrix_print(int m,int n,int *c)
-{
-	for(int i=0;i<m;i++)
-	{
-		for(int j=0;j<n;j++)
-		printf("%4d",*c++);
-		printf("\n");
-	}
-}
-
 
